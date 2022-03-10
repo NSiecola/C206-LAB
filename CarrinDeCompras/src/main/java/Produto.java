d06b0a41705cb1ee15d363d5a7919a7276ae2dd9
Nicholas do Vale Siécola <100312825+NSiecola@users.noreply.github.com>
2022-03-10 02:47:14 -0300

    Add files via upload

---
+++ CarrinDeCompras/src/main/java/Produto.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Produto {
+        Scanner input = new Scanner(System.in);
+
+        String nome, descri, fabricante;
+        double cust;
+
+        void inserirProduto(){
+                System.out.println("Qual o nome do produto?");
+                nome = input.nextLine();
+                System.out.println("Qual a descrição do produto?");
+                descri = input.nextLine();
+                System.out.println("Qual o fabricante do produto?");
+                fabricante = input.nextLine();
+                System.out.println("Qual o preço do produto?");
+                cust = input.nextInt();
+                input.nextLine();
+        }
+
+        void mostraInfo(){
+                System.out.println("Nome do produto: " + nome);
+                System.out.println("Descrição do produto: " + descri);
+                System.out.println("Fabricante do produto: " + fabricante);
+                System.out.println("Preço do produto: " + cust);
+        }
+}
